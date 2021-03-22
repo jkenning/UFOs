26c0e9ee99a6d6534127d6d693a61070f876f5ee
Jack Kenning <jack.j.kenning@gmail.com>
2021-03-22 18:50:28 -0500

    Update app.js

--- static/js/app.js
+++ static/js/app.js
@@ -9,14 +9,21 @@ addition = (a, b) => a + b;
 doubleAddition = (c, d) => addition(c, d) * 2;
 
 function buildTable(data) {
+    // First, clear out any existing data
     tbody.html("");
-  }
-
+  
+    // Next, loop through each object in the data
+    // and append a row and cells for each value in the row
     data.forEach((dataRow) => {
-        let row = tbody.append("tr");
-        Object.values(dataRow).forEach((val) => {
+      // Append a row to the table body
+      let row = tbody.append("tr");
+  
+      // Loop through each field in the dataRow and add
+      // each value as a table cell (td)
+      Object.values(dataRow).forEach((val) => {
         let cell = row.append("td");
         cell.text(val);
         }
-        );
+      );
     });
+  }
